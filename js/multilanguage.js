49428c7e628c723ee1b1d2c73a547877185fac2e
Ruben <r.ramosfg@gmail.com>
2017-02-16 15:24:28 +0100

    Modificacion script multilenguaje
    Con las modificaciones realizadas para coger todo el texto bien

--- js/multilanguage.js
+++ js/multilanguage.js
@@ -1,13 +1,16 @@
-$(document).ready(function() {
-  $('.texto_ingles').hide();
-  $('.banderas > a').click(function() {
-    changeLanguage($(this).attr("class"), $(this));
+<script crossorigin="anonymous" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.texto_ingles').hide();
+    $('.banderas > a').click(function() {
+      changeLanguage($(this).attr("class"), $(this));
+    });
   });
-});
 
-function changeLanguage(lang, obj) {
-  var parent = obj.closest('.father');
+  function changeLanguage(lang, obj) {
+    var parent = obj.closest('.father');
 
-  parent.find('.contenido > div').hide();
-  parent.find('.texto_' + lang).show();
-}
+    parent.find('.contenido > div').hide();
+    parent.find('.texto_' + lang).show();
+  }
+</script>
